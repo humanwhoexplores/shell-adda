3c0e970bfa4573c92a0a412275c1ff586104ce59
krchopra <krishan.chopra@oracle.com>
2023-01-22 17:03:09 +0530

    shell script to check if a file can be read.

---
+++ can-you-read-the-file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# checking if the file exists and if it is readable 
+
+filename=/etc/passwd
+
+echo "Checking if file exists"
+
+if [ -f $filename ]
+then
+    echo "File Exists"
+    if [ -r $filename ]
+    then
+        echo "file can be read"
+        cat $filename
+    else
+        echo "No read permission on file."
+    fi 
+else
+    echo "file does not exist"
+fi
